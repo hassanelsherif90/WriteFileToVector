615e146782cf766583e0a7621eac3a3199369bf3
Hassan El Sherif <114035578+hassanelsherif90@users.noreply.github.com>
2023-04-20 18:07:25 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+void LoadFileToVector(string Path, vector<string> &vFileContent)
+{
+    fstream MyFile;
+
+    MyFile.open(Path, ios::in);
+
+    if (MyFile.is_open())
+    {
+        string Line;
+        while (getline(MyFile, Line))
+        {
+            vFileContent.push_back(Line);
+        }
+        MyFile.close();
+    }
+}
+
+int main()
+{
+    vector<string> vFileContent;
+    LoadFileToVector("MyFile.txt", vFileContent);
+
+    for (string &Line : vFileContent)
+    {
+        cout << Line << endl;
+    }
+
+    return 0;
+}
