7976cf802afbad26d0572afa07cf0582abec0bf0
Hassan El Sherif <114035578+hassanelsherif90@users.noreply.github.com>
2023-04-25 14:55:39 +0200

    Update

--- StrDate.cpp
+++ StrDate.cpp
@@ -6,16 +6,18 @@ using namespace std;
 int main()
 {
     time_t t = time(0);
-    tm * Now = localtime(&t);
+    tm *Now = localtime(&t);
     cout << "Year   : " << Now->tm_year + 1900 << endl;
     cout << "Mounth : " << Now->tm_mon + 1 << endl;
     cout << "Day    : " << Now->tm_mday << endl;
     cout << "Hour   : " << Now->tm_hour << endl;
-    cout << "Min    : " << Now->tm_min  << endl;
+    cout << "Min    : " << Now->tm_min << endl;
     cout << "Second : " << Now->tm_sec << endl;
     cout << "Week Day (Days since sunday)  : " << Now->tm_wday << endl;
     cout << "Year Day (Days since Jan 1st) : " << Now->tm_yday << endl;
-    cout << "Hour of Daylight saving time  : " << Now->tm_isdst<< endl;
-    
+    cout << "Hour of Daylight saving time  : " << Now->tm_isdst << endl;
+    cout << "Please Enter any Key To end : ";
+    int i;
+    cin >> i;
     return 0;
 }
